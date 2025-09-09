5d6bdc650ff79032249b7c55dd98fd86f4d4f752
Majeeee <Majid.Tajik@stud.sti.se>
2025-09-09 20:12:09 +0200

    Initial commit: First commit!

---
+++ webService_projekt/src/main/java/se/sti/webservice/webService_projekt/WebServiceProjektApplication.java
@@ -0,0 +1,13 @@
+package se.sti.webservice.webService_projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceProjektApplication.class, args);
+	}
+
+}
---
+++ webService_projekt/src/test/java/se/sti/webservice/webService_projekt/WebServiceProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package se.sti.webservice.webService_projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
