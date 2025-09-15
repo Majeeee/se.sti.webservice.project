727ea0c277d4e01deae6c94ca2976828c72161da
Majeeee <Majid.Tajik@stud.sti.se>
2025-09-15 11:37:26 +0200

    US1: Add Todo model and repository,Gradle Kotlin DSL
    Add getter and setter

--- webService_projekt/src/main/java/se/sti/webservice/webService_projekt/model/Todo.java
+++ webService_projekt/src/main/java/se/sti/webservice/webService_projekt/model/Todo.java
@@ -11,4 +11,17 @@ public class Todo {
     private boolean completed;
 
     // getters and setters
+    public Todo(String title, String description, boolean completed) {
+        this.title = title;
+        this.completed = completed;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public boolean isCompleted() { return completed; }
+    public void setCompleted(boolean completed) { this.completed = completed; }
 }
